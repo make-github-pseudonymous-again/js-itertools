261ac54a56a5d6169993601f2daae42b1446babb
aureooms <aurelien.ooms@gmail.com>
2014-10-09 23:06:13 +0200

    some tests

---
+++ test/js/src/any.js
@@ -0,0 +1,26 @@
+
+var one = function (obj, val) {
+	deepEqual(itertools.any(obj), val, "any " + JSON.stringify(obj));
+};
+
+
+test("any", function () {
+
+	one([], false);
+	one([true], true);
+	one([false], false);
+
+	one([true, true, true, true], true);
+	one([false, false, false, false], false);
+
+	one([false, true, true, true], true);
+	one([true, false, true, true], true);
+	one([true, true, false, true], true);
+	one([true, true, true, false], true);
+
+	one([true, false, false, false], true);
+	one([false, true, false, false], true);
+	one([false, false, true, false], true);
+	one([false, false, false, true], true);
+
+});
--- test/js/src/dummy.js
+++
@@ -1,4 +0,0 @@
-
-test("dummy", function () {
-	ok(false, "tests implemented");
-});
