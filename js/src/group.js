3695adf481d11f1aa565f169e11c7d8bdffbe3ae
aureooms <aurelien.ooms@gmail.com>
2014-10-13 09:42:14 +0200

    draft for group

---
+++ js/src/group.js
@@ -0,0 +1,45 @@
+
+var group = function ( iterable, key, out ) {
+
+	var i, len, curr, next, tuple, item, items;
+
+	len = iterable.length;
+
+	if ( len === 0 ) {
+		return out;
+	}
+
+	i = 0;
+	item = iterable[i];
+	next = key( item );
+
+	do {
+		curr = next;
+		items = [item];
+		tuple = [curr, items];
+
+		++i;
+
+		while ( i < len ) {
+
+			item = iterable[i];
+			next = key( item );
+
+			if ( next !== curr ) {
+				break;
+			}
+
+			items.push( item );
+			++i;
+		}
+
+		out.push( tuple );
+
+	} while ( i < len );
+
+
+	return out;
+
+};
+
+exports.group = group;
