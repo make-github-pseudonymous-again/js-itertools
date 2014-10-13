1529a5c740e9ad25dad383ba13f2c5387eee2c0c
aureooms <aurelien.ooms@gmail.com>
2014-10-13 11:58:52 +0200

    draft for combinations with repetition

---
+++ js/src/combinationswithrepetition.js
@@ -0,0 +1,44 @@
+
+
+var combinationswithrepetition = function ( iterable, r, out ) {
+
+	// combinations_with_replacement('ABC', 2) --> AA AB AC BB BC CC
+
+	var pool, len, indices, i, next;
+
+	pool = iterable;
+	len = pool.length;
+
+	if ( len === 0 && r > 0) {
+		return out;
+	}
+
+	indices = repeat( 0, r );
+
+	out.push( pick( pool, indices, [] ) );
+
+	for ( ; ; ) {
+
+		for ( i = r - 1 ; i >= 0 ; --i ) {
+			if ( indices[i] !== len - 1 ) {
+				next = indices[i] + 1;
+				break;
+			}
+		}
+
+		if ( i < 0 ) {
+			return out;
+		}
+
+		for ( ; i < r ; ++i ) {
+			indices[i] = next;
+		}
+
+		out.push( pick( pool, indices, [] ) );
+	}
+
+	return out;
+
+};
+
+exports.combinationswithrepetition = combinationswithrepetition;
