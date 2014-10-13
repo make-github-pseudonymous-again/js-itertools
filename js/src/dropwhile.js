ac99531fab6ed2f6096fb772bfdc837a011d88c1
aureooms <aurelien.ooms@gmail.com>
2014-10-13 09:41:41 +0200

    draft for dropwhile

---
+++ js/src/dropwhile.js
@@ -0,0 +1,35 @@
+
+var dropwhile = function ( predicate, iterable, out ) {
+
+	var i, n;
+
+	n = iterable.length;
+
+	if ( n === 0 ) {
+		return out;
+	}
+
+	i = 0;
+	e = iterable[i];
+
+	while ( predicate( e ) ) {
+		++i;
+
+		if ( i === n ) {
+			return out;
+		}
+
+		e = iterable[i];
+	}
+
+	out.push( e );
+
+	for ( ++i ; i < n ; ++i ) {
+		out.push( iterable[i] );
+	}
+
+	return out;
+
+};
+
+exports.dropwhile = dropwhile;
