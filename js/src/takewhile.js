177da71f49ef1fe2b6e4c513f78721b3307429d2
aureooms <aurelien.ooms@gmail.com>
2014-10-13 09:43:47 +0200

    draft for takewhile

---
+++ js/src/takewhile.js
@@ -0,0 +1,32 @@
+
+var takewhile = function ( predicate, iterable, out ) {
+
+	var i, n;
+
+	n = iterable.length;
+
+	if ( n === 0 ) {
+		return out;
+	}
+
+	i = 0;
+	e = iterable[i];
+
+	while ( predicate( e ) ) {
+
+		out.push( e );
+
+		++i;
+
+		if ( i === n ) {
+			return out;
+		}
+
+		e = iterable[i];
+	}
+
+	return out;
+
+};
+
+exports.takewhile = takewhile;
