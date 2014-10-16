9dc8fac2c3fd750ec45214ace61942ec9d226ad6
aureooms <aurelien.ooms@gmail.com>
2014-10-16 10:02:10 +0200

    tests for min and max

---
+++ test/js/src/min.js
@@ -0,0 +1,33 @@
+
+
+var one, sort;
+
+sort = require ( "aureooms-js-sort" );
+
+one = function ( iterable, f, expected ) {
+
+	deepEqual( itertools.min( iterable, f ), expected, JSON.stringify( iterable ) );
+
+};
+
+test( "min", function () {
+
+	one( [], sort.increasing, undefined );
+	one( [1], sort.increasing, 1 );
+	one( [1, 2, 3], sort.increasing, 1 );
+	one( [3, 2, 1], sort.increasing, 1 );
+	one( [2, 3, 1], sort.increasing, 1 );
+	one( [1, 3, 2], sort.increasing, 1 );
+	one( [2, 1, 3], sort.increasing, 1 );
+	one( [3, 1, 2], sort.increasing, 1 );
+
+	one( [], sort.decreasing, undefined );
+	one( [1], sort.decreasing, 1 );
+	one( [1, 2, 3], sort.decreasing, 3 );
+	one( [3, 2, 1], sort.decreasing, 3 );
+	one( [2, 3, 1], sort.decreasing, 3 );
+	one( [1, 3, 2], sort.decreasing, 3 );
+	one( [2, 1, 3], sort.decreasing, 3 );
+	one( [3, 1, 2], sort.decreasing, 3 );
+
+});
