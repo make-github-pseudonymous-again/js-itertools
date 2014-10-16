b8219bc58afb4785e5ca4847c1d11988453efeb9
aureooms <aurelien.ooms@gmail.com>
2014-10-16 09:41:42 +0200

    drafts for min and max

---
+++ js/src/min.js
@@ -0,0 +1,28 @@
+
+var min = function ( iterable, f ) {
+
+	var a, b, i, len;
+
+	len = iterable.length;
+
+	if ( len === 0 ) {
+		return undefined;
+	}
+
+	a = iterable[0];
+
+	for ( i = 1 ; i < len ; ++i ) {
+
+		b = iterable[i];
+
+		if ( f( b, a ) < 0 ) {
+			a = b;
+		}
+
+	}
+
+	return a;
+
+};
+
+exports.min = min;
