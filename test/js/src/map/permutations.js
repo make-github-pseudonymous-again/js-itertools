219dc56c1d85ed75d1f1badd489af47ee7b4b023
aureooms <aurelien.ooms@gmail.com>
2014-10-16 11:19:05 +0200

    test for permutations

---
+++ test/js/src/map/permutations.js
@@ -0,0 +1,37 @@
+
+
+
+var one;
+
+one = function ( iterable, repeat, expected ) {
+
+
+	deepEqual(
+		itertools.permutations( iterable, repeat, [] ),
+		expected,
+		JSON.stringify( [iterable, repeat] )
+	);
+
+};
+
+
+
+test( "permutations", function () {
+
+	one(
+		"ABCD",
+		2,
+		[
+			["A","B"], ["A","C"], ["A","D"], ["B","A"], ["B","C"], ["B","D"],
+			["C","A"], ["C","B"], ["C","D"], ["D","A"], ["D","B"], ["D","C"]
+		]
+	);
+
+	one(
+		itertools.range( 0, 3, 1, [] ),
+		3,
+		[ [0,1,2], [0,2,1], [1,0,2], [1,2,0], [2,0,1], [2,1,0] ]
+	);
+
+});
+
