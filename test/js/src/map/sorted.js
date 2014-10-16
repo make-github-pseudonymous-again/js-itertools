0ecb97b25de0c9c3768b2aa5e013918a769a1f85
aureooms <aurelien.ooms@gmail.com>
2014-10-16 15:07:17 +0200

    test for sorted

--- test/js/src/map/sorted.js
+++ test/js/src/map/sorted.js
@@ -1,3 +1,56 @@
 
+var one, sort, util, operator;
 
-// TODO when aureooms-js-sort is complete
+util = require( "util" );
+
+sort = require( "aureooms-js-sort" );
+
+operator = require( "aureooms-js-operator" );
+
+one = function ( f, a ) {
+
+	var b, i, initializer;
+
+	b = itertools.sorted( f, a );
+
+	deepEqual( b.length, a.length, "length check" );
+
+	initializer = {};
+
+	itertools.reduce( function ( x, y ) {
+
+		if ( x !== initializer ) {
+
+			ok( f( x, y ) <= 0, util.format( "f( %f, %f ) <= 0", x, y ) );
+
+		}
+
+		ok( operator.contains( a, y ), util.format( "%f in %s", y, JSON.stringify( a ) ) );
+
+		return y;
+
+	}, b, initializer );
+
+};
+
+test( "sorted", function () {
+
+	[sort.increasing, sort.decreasing].forEach( function (f) {
+
+		var a, i, n;
+
+		a = [];
+
+		n = 100;
+
+		i = n;
+
+		while ( i-- ) {
+			a.push( Math.random() );
+		}
+
+		one( f, a );
+
+	});
+
+});
