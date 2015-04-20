e6b14c2ffc1fd12a932173868c8264fc0b9782b4
aureooms <aurelien.ooms@gmail.com>
2015-04-20 13:44:41 +0200

    rewrite chain using es6

--- js/src/map/chain.js
+++ js/src/map/chain.js
@@ -1,28 +1,8 @@
 
-var chain = function ( iterables, out ) {
+var chain = function* ( iterables ) {
 
-	var i, j, n, len, it;
+	for ( let iterable of iterables ) yield* iterable ;
 
-	len = iterables.length;
-
-
-	if ( len === 0 ) {
-		return out;
-	}
-
-
-	for ( i = 0 ; i < len ; ++i ) {
-
-		it = iterables[i];
-		n = it.length;
-
-		for ( j = 0 ; j < n ; ++j ) {
-			out.push( it[j] );
-		}
-	}
-
-	return out;
-
-};
+} ;
 
 exports.chain = chain;
