103afcda6c475e1f83391c99a9012ef9acc59fdc
aureooms <aurelien.ooms@gmail.com>
2016-04-01 20:51:27 +0200

    add documentation to product

--- src/map/product.js
+++ src/map/product.js
@@ -2,14 +2,20 @@
 import { list , map , _product , ncycle , reversed } from '..' ;
 
 /**
+ * Computes the product of the iterables given as first parameter. The second
+ * is an integer that tells how many times the list of iterables given as input
+ * should be concatenated to itself before computing the product.
  *
- * @example product(['ABCD', 'xy'], 1) // Ax Ay Bx By Cx Cy Dx Dy
+ * @param {iterable} iterables input iterables
+ * @param {Number} repeat number of times to cycle through the input iterables
+ * @example product(['ABCD', 'xy']) // Ax Ay Bx By Cx Cy Dx Dy
  * @example product([range(0, 2, 1)], 3) // 000 001 010 011 100 101 110 111
+ * @return {Generator}
  */
 
-export function product ( iterables , r ) {
+export function product ( iterables , repeat = 1 ) {
 
-	let pools = list( ncycle( reversed( map( list , iterables ) ) , r ) ) ;
+	let pools = list( ncycle( reversed( map( list , iterables ) ) , repeat ) ) ;
 
 	return map( list , _product( pools , 0 , pools.length ) ) ;
 
