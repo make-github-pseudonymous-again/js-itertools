7da2020f1d18b859373250f3f1f7652210b62ec2
aureooms <aurelien.ooms@gmail.com>
2016-07-19 11:58:46 +0200

    upgrade ava, nyc and use Array.from

--- src/base/list.js
+++ src/base/list.js
@@ -1,11 +1,5 @@
 export function list ( iterable ) {
 
-	// replace by return [ ...iterable ] ;
-
-	let buffer = [ ] ;
-
-	for ( let item of iterable ) buffer.push( item ) ;
-
-	return buffer ;
+	return Array.from( iterable ) ;
 
 }
