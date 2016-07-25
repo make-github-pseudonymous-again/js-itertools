e5e97d663d5ece80da5f8531fa6d237c3fbe02f9
Aurélien Ooms <aurelien.ooms@gmail.com>
2016-07-25 19:31:30 +0200

    Document roundrobin

--- src/map/roundrobin.js
+++ src/map/roundrobin.js
@@ -1,12 +1,20 @@
 import { iter , len , cycle , map , slice } from '..' ;
 
 /**
- * Recipe credited to George Sakkis
+ * Yields the first item of the first input iterable, then the first item of
+ * the second input iterable, etc., until the last input iterable. Then start
+ * again with the second item of the first input iterable, etc. If one of the
+ * input iterable is exhausted, it is removed from the list of input iterables
+ * and the algorithm continues until all input iterables have been exhausted.
  *
- * @example roundrobin(['ABC', 'D', 'EF']) // A D E B F C
+ * @example
+ * // returns ['A','D','E','B','F','C]
+ * list( roundrobin(['ABC', 'D', 'EF']) )
+ *
+ * @param {Iterable[]} iterables - The input iterables.
+ * @returns {Iterator}
  *
  */
-
 export function* roundrobin ( iterables ) {
 
 	let pending = len( iterables ) ;
