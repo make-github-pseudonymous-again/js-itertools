3d0704c2609003a519c0019a43cf096facc58686
Aurélien Ooms <aurelien.ooms@gmail.com>
2016-07-27 09:55:32 +0200

    test by with infinite generators

--- test/src/base/by.js
+++ test/src/base/by.js
@@ -1,12 +1,8 @@
 import test from 'ava' ;
 
-	 import {
-		 range ,
-		 by ,
-		 zip ,
-		 list } from '../../..' ;
-test( 'by' , t => {
+import { range , by , zip , list , count , head } from '../../..' ;
 
+test( 'by' , t => {
 
 	const A = by( range( 100 ) , 2 ) ;
 	const B = zip( range( 0 , 100 , 2 ) , range( 1 , 100 , 2 ) ) ;
@@ -20,6 +16,15 @@ test( 'by' , t => {
 	const D = by( '' , 3 ) ;
 
 	t.deepEqual( list( D ) , [ ] , 'empty' ) ;
+
+} ) ;
+
+test( 'by (infinite sequences)' , t => {
+
+	const A = by( count( ) , 2 ) ;
+	const B = zip( count( 0 , 2 ) , count( 1 , 2 ) ) ;
+
+	t.deepEqual( list( head( A , 1000 ) ) , list( head( B , 1000 ) ) , 'compare to zip output' ) ;
 
 } ) ;
 
