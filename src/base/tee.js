f81f9d51054e0f9f3894695a64535d739b5d421c
Aurélien Ooms <aurelien.ooms@gmail.com>
2016-07-27 16:53:07 +0200

    use real deque in tee (fix #2)

--- src/base/tee.js
+++ src/base/tee.js
@@ -2,6 +2,8 @@ import { iter } from './iter' ;
 import { list } from './list' ;
 import { map } from '../map/map' ;
 
+import { deque } from 'aureooms-js-collections' ;
+
 /**
  * Returns <code>n</code> copies of the input iterable. Note that if the input
  * iterable is an iterator, then it must be discarded by the caller after
@@ -15,31 +17,30 @@ export function tee ( iterable , n ) {
 
 	let iterator = iter( iterable ) ;
 
-	// use real deque here
-	let deques = [ ] ;
+	let copies = [ ] ;
 
-	while ( n --> 0 ) deques.push( [ ] ) ;
+	while ( n --> 0 ) copies.push( deque( ) ) ;
 
-	let gen = function* ( mydeque ) {
+	let gen = function* ( mycopy ) {
 
 		while ( true ) {
 
-			if ( mydeque.length === 0 ) {
+			if ( mycopy.length === 0 ) {
 
 				let current = iterator.next() ;
 
 				if ( current.done ) return ;
 
-				for ( let deque of deques ) deque.push( current.value ) ;
+				for ( let copy of copies ) copy.append( current.value ) ;
 
 			}
 
-			yield mydeque.shift( ) ;
+			yield mycopy.popleft( ) ;
 
 		}
 
 	} ;
 
-	return list( map( gen , deques ) ) ;
+	return list( map( gen , copies ) ) ;
 
 }
