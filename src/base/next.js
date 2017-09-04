9c7bc3374b745d99b8d338764434f0546a75dd1e
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-09-04 15:42:38 +0900

    fix #33

--- src/base/next.js
+++ src/base/next.js
@@ -1,17 +1,23 @@
 import { StopIteration } from './StopIteration' ;
 
 /**
- * Returns the next value of the input iterator. If there the iterator is
- * exhausted, throws a {@link StopIteration}.
+ * Returns the next value of the input iterator. If the iterator is exhausted,
+ * throws a {@link StopIteration}, unless a <code>dflt</code> argument has been
+ * specified, in which case the <code>dflt</code> argument is returned.
  *
  * @param {Iterator} iterator - The input iterator.
+ * @param {Object} [dflt=undefined] - The default value to return in the case
+ * that the input iterator is exhausted.
  * @returns {Object}
  */
-export function next ( iterator ) {
+export function next ( iterator , dflt = undefined ) {
 
 	const x = iterator.next( ) ;
 
-	if ( x.done ) throw new StopIteration();
+	if ( x.done ) {
+		if ( dflt === undefined ) throw new StopIteration() ;
+		else return dflt ;
+	}
 
 	return x.value ;
 
--- test/src/base/next.js
+++ test/src/base/next.js
@@ -4,12 +4,16 @@ import { range , next , StopIteration } from '../../../src' ;
 
 test( "next" , t => {
 
+	const dflt = {} ;
+
 	t.throws( ( ) => next( range( 0 ) ) , StopIteration , 'next on empty iterator throws StopIteration' ) ;
+	t.is( next( range( 0 ) , dflt ) , dflt , 'next on empty iterator with default returns default' ) ;
 
 	const it = range( 57 , 14 , -3 ) ;
 
 	t.deepEqual( next( it ) , 57 ) ;
 	t.deepEqual( next( it ) , 54 ) ;
 	t.deepEqual( next( it ) , 51 ) ;
+	t.deepEqual( next( it , dflt ) , 48 ) ;
 
 } ) ;
