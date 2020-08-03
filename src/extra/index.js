fc11fd1e1fc0ce85ac3832859c874c9f5d19d0f6
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-08-03 15:01:07 +0200

    :sparkles: feat(extra/nth): Add nth(iterable, n).
    Fixes #39.

--- src/extra/index.js
+++ src/extra/index.js
@@ -1,1 +1,2 @@
 export * from './closure';
+export * from './nth';
---
+++ src/extra/nth.js
@@ -0,0 +1,27 @@
+import deque from '@aureooms/js-collections-deque';
+import {next, drop, StopIteration} from '../base';
+
+/**
+ * Returns the <code>n</code>th value of the input iterable (<code>n</code>
+ * is a 0-based index). If
+ * <code>n</code> is negative, returns the <code>n</code>th value from the
+ * end.
+ * If no such value exists, throws a {@link StopIteration}.
+ *
+ * @example
+ * // returns 3
+ * nth( range( 5 ) , 3 ) ;
+ *
+ * @param {Iterable} iterable - The input iterable.
+ * @param {Number} n - The index of the value to output.
+ * @returns {Object} The <code>n</code>th value of the input iterable.
+ */
+export function nth(iterable, n) {
+	if (n < 0) {
+		const deq = deque(iterable, -n);
+		if (deq.len() < -n) throw new StopIteration();
+		return deq.get(0);
+	}
+
+	return next(drop(iterable, n));
+}
